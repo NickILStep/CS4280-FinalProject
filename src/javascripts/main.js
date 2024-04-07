82e1430e6bebc4a733646254254d532515148a41
NickStepWeber <113000993+NickStepWeber@users.noreply.github.com>
2024-04-06 21:31:08 -0600

    First commit

---
+++ src/javascripts/main.js
@@ -0,0 +1,65 @@
+import * as THREE from 'three'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import * as dat from 'dat.gui'
+
+// Required by Webpack - do not touch
+require.context('../', true, /\.(html|json|txt|dat)$/i)
+require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
+require.context('../stylesheets/', true, /\.(css|scss)$/i)
+
+// First: Set up your name
+let std_name = "Chance Hardman & Nicholas Stephenson"
+document.querySelector('#std_name').innerHTML = `<strong>${std_name}</strong>`
+
+//Then: comes everything else
+// Setup and controls
+let canvas = document.querySelector('#webgl-scene')
+let scene = new THREE.Scene()
+let renderer = new THREE.WebGLRenderer({canvas})
+let camera = new THREE.PerspectiveCamera(45, canvas.clientWidth / canvas.clientWidth, .1, 1000)
+
+renderer.setSize(canvas.clientWidth, canvas.clientHeight)
+renderer.setClearColor(0xEEEEEE)
+
+let axes = new THREE.AxesHelper(10)
+scene.add(axes)
+
+let cameraControls = new OrbitControls(camera, renderer.domElement)
+cameraControls.addEventListener("change", function(){
+    renderer.render(scene, camera)
+})
+
+let controls = {
+    radius: 400,
+    theta: 1,
+    phi: 1
+}
+
+// Lighting
+let ambientLight = new THREE.AmbientLight(0xFFFFFF)
+ambientLight.intensity = .9
+scene.add(ambientLight)
+
+// Floor
+let floorGeo = new THREE.PlaneGeometry(50, 35)
+let floorMat = new THREE.MeshPhongMaterial()
+
+let floor = new THREE.Mesh(floorGeo, floorMat)
+
+floor.position.set(0, 0, 0)
+floor.material.color = new THREE.Color(1, 0, 1)
+floor.rotateX(-Math.PI / 2)
+scene.add(floor)
+
+// Render
+function animate() {
+    camera.position.x = controls.radius * Math.sin(controls.theta) * Math.cos(controls.phi)
+    camera.position.y = controls.radius * Math.cos(controls.theta)
+    camera.position.z = controls.radius * Math.sin(controls.theta) * Math.sin(controls.phi)
+
+    camera.lookAt(scene.position)
+    renderer.render(scene, camera)
+    cameraControls.update()
+}
+
+animate()
